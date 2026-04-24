f1e8547fa5ff1bb2302c6ded04b2d441c7eb0a7f
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-04-24 21:02:00 +0700

    feat: close modal

--- laravel/resources/views/livewire/pages/notification.blade.php
+++ laravel/resources/views/livewire/pages/notification.blade.php
@@ -1,12 +1,16 @@
-<div x-data="{ open: true, selected: null }">
-    <div x-show="open" class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+<div x-data="{ isOpenModal: true }">
+    <div x-show="isOpenModal" class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
         <!-- Modal -->
         <div class="bg-white w-full max-w-4xl h-[80vh] rounded-2xl shadow-xl flex overflow-hidden">
             <!-- LEFT: Notification List -->
             <div class="w-1/3 border-r overflow-y-auto">
 
-                <div class="p-4 font-semibold border-b">
-                    🔔 Notifikasi
+                <div class="flex justify-between items-center p-4 pr-2 font-semibold border-b">
+                    <h2>🔔 Notifikasi</h2>
+                    <button type="button" @click="isOpenModal = false"
+                        class="cursor-pointer rounded-md p-1 text-gray-700 hover:bg-gray-50">
+                        <x-icons.cross size="20" />
+                    </button>
                 </div>
 
                 <!-- LEFT: Notifications -->
@@ -27,42 +31,49 @@
 
             <!-- RIGHT: Detail -->
             <div class="flex-1 p-6 overflow-y-auto">
-                <!-- Detail 1 -->
-                <div wire:loading wire:target='detailNotification' class="w-full h-full">
-                    <x-icons.loading size="48" class="m-auto" />
+                <div wire:loading wire:target='detailNotification' class="w-full h-[98%]">
+                    <x-icons.loading size="48" class="m-auto h-full" />
                 </div>
                 <div wire:loading.remove wire:target='detailNotification' class="space-y-4">
-                    <h2 class="text-lg font-semibold text-red-600">
-                        ⚠️ {{ $active_notification['title'] }}
-                    </h2>
 
+                    {{-- Title --}}
+                    <div class="flex justify-between items-center gap-x-2">
+                        <h2 class="text-lg font-semibold text-red-600">
+                            ⚠️ {{ $active_notification['title'] }}
+                        </h2>
+                        {{-- Time --}}
+                        <span class="text-sm text-gray-800">{{ $active_notification['created_at'] }}</span>
+                        {{-- Message --}}
+                    </div>
                     <p class="text-sm text-gray-600">
                         {{ $active_notification['message'] }}
                     </p>
-
                     <div class="bg-gray-50 p-4 rounded-lg text-sm">
+                        {{-- Value --}}
                         <div class="flex justify-between">
                             <span>{{ ucwords($active_notification['sensor_type']) }}</span>
                             <span class="font-semibold text-red-600">{{ $active_notification['value'] }}</span>
                         </div>
+                        {{-- Threshold --}}
                         <div class="flex justify-between">
                             <span>Threshold</span>
                             <span>{{ $active_notification['threshold'] }}</span>
                         </div>
                     </div>
 
+                    {{-- Recomendation --}}
                     <div class="bg-green-50 p-3 rounded-lg text-sm text-green-700">
                         💡 Rekomendasi: {{ $active_notification['recomendation'] }}
                     </div>
 
                     <div class="flex gap-2">
                         @if (!$active_notification['is_read'])
-                            <button class="px-4 py-2 bg-gray-200 rounded-lg">
+                            <button class="px-4 py-2 bg-gray-200 rounded-lg cursor-pointer">
                                 Tandai Dibaca
                             </button>
                         @endif
                         @if ($active_notification['is_active'])
-                            <button class="px-4 py-2 bg-blue-600 text-white rounded-lg">
+                            <button class="px-4 py-2 bg-blue-600 text-white rounded-lg cursor-pointer">
                                 Resolve
                             </button>
                         @endif
