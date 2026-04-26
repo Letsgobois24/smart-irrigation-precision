7705da56b2dbb0e508a9a2e7b80679829bd8260e
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-04-26 20:14:15 +0700

    feat: generate random data

---
+++ iot/data.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+node_data = {
+  "measurement": 'node',
+  "node_id": "node_1",
+  "time": time.time(),
+  "trees": [getTree(i) for i in range(1, 5)]
+}
+
+global_data = {
+    "measurement": 'global',
+    "ph": round(random.uniform(6, 8), 2),
+    "water_flow": round(random.uniform(0.1, 1), 2),
+    "main_valve": random.choice([True, False]),
+    "time": time.time()
+}
+
+def getTree(tree_id: int):
+  return {
+    "soil_moisture": round(random.uniform(50, 80), 1),
+    "tree_id": tree_id,
+    "valve": random.choice([True, False])
+}
