7d6dd13cc62f50d160b3558f32e6684f1aecb8f3
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-05-10 19:53:59 +0700

    feat: make system event seeder

---
+++ laravel/database/seeders/SystemEventSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Services\InfluxDBService;
+use Carbon\CarbonPeriod;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Throwable;
+
+class SystemEventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(InfluxDBService $influx): void
+    {
+        $rows = [];
+
+        $end = now()->startOfHour();
+        $start = (clone $end)->subDays(2);
+        $randomMinutes = random_int(60, 180);
+        $period = CarbonPeriod::create($start, "$randomMinutes minutes", $end);
+
+        foreach ($period as $date) {
+            $valve_status = fake()->boolean(50);
+            $current_before = $valve_status ? 0 : fake()->randomFloat(1, 100, 120);
+            $current_after = !$valve_status ? 0 : fake()->randomFloat(1, 100, 120);
+
+            $rows[] = [
+                'measurement' => 'system_event',
+                'tags' => [
+                    'node_id' => 1,
+                    'tree_id' => random_int(1, 4)
+                ],
+                'fields' => [
+                    'valve' => $valve_status,
+                    'current_t' => $current_before,
+                    'current_avg' => ($current_before + $current_after) / 2,
+                    'current_t+2s' => $current_after,
+                    'moisture_t' => $valve_status ? fake()->randomFloat(1, 50, 60) : fake()->randomFloat(1, 70, 80),
+                    'moisture_t+10m' => !$valve_status ? fake()->randomFloat(1, 50, 60) : fake()->randomFloat(1, 70, 80),
+                    'flow_t' => $valve_status ? fake()->randomFloat(1, 0.1, 0.5) : fake()->randomFloat(1, 1, 2),
+                    'duration' => 600,
+                ],
+                'time' => $date->getTimestamp()
+            ];
+        }
+
+        try {
+            $influx->storeMultiple(rows: $rows);
+        } catch (Throwable $e) {
+            dump("Connection Timeout. Detail: " . $e->getMessage());
+        }
+    }
+}
