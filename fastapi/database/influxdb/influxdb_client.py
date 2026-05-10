debb07e4c2cd1ec0dc5fbc94489230a0b55772d2
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-05-10 21:33:22 +0700

    fix: table column tags

--- fastapi/database/influxdb/influxdb_client.py
+++ fastapi/database/influxdb/influxdb_client.py
@@ -22,14 +22,14 @@ def getSensorData(limit: int):
     table['time'].astype('int')
     return table.to_json(orient='records')
 
-def addData(data: Dict[str, float], measurement: str):
+def addData(data: Dict[str, float], measurement: str, tags: list | None = None):
     df = pd.DataFrame(data=[data])
     df['time'] = convertTimeToSecond(df['time'])
-    client.write_dataframe(df=df, measurement=measurement, timestamp_column='time')
+    client.write_dataframe(df=df, measurement=measurement, timestamp_column='time', tags=tags)
     
-def extendData(df: pd.DataFrame, measurement: str):
+def extendData(df: pd.DataFrame, measurement: str, tags: list | None = None):
     df['time'] = convertTimeToSecond(df['time'])
-    client.write_dataframe(df=df, measurement=measurement, timestamp_column='time', tags=['tree_id', 'node_id'])
+    client.write_dataframe(df=df, measurement=measurement, timestamp_column='time', tags=tags)
 
 def convertTimeToSecond(df_column):
     return pd.to_datetime(df_column, unit='s').astype('datetime64[s]')
