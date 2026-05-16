e57f8c1b57be1f28654dc7ab555178a7b866fafa
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-05-16 19:48:00 +0700

    refactor: node card

--- laravel/resources/views/livewire/components/node-monitoring.blade.php
+++ laravel/resources/views/livewire/components/node-monitoring.blade.php
@@ -47,33 +47,9 @@
         </div>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-6">
-
             <!-- 4 Tree -->
             @foreach ($node_data as $tree)
-                <div class="bg-green-50 p-3 rounded-lg">
-                    <div class="flex items-center gap-2 mb-1">
-                        🌱 <p class="text-sm text-gray-500">Tree {{ $tree['tree_id'] }}</p>
-                    </div>
-
-                    <!-- Moisture -->
-                    <div class="flex items-center gap-2">
-                        <!-- icon kelembaban -->
-                        <x-icons.humidity size="20" class="text-blue-500" />
-                        <p class="font-bold">{{ $tree['soil_moisture'] }}%</p>
-                    </div>
-
-                    <!-- Valve -->
-                    <div class="flex items-center gap-1 mt-1">
-                        ⚙️
-                        <p class="text-xs text-green-600">Valve {{ $tree['valve'] ? 'ON' : 'OFF' }}</p>
-                    </div>
-
-                    <!-- Detail Update -->
-                    <div class="mt-2 text-xs text-gray-400 flex flex-wrap gap-x-1">
-                        <span>Last update:</span>
-                        <span>{{ smartTimeFormat($tree['time']) }}</span>
-                    </div>
-                </div>
+                <x-card.node :tree="$tree" />
             @endforeach
         </div>
 
