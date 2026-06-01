28c3d38dddf1b049ed7ed717b7ead790d62a27e3
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-06-01 19:12:50 +0700

    refactor: show all button 2

--- laravel/resources/views/livewire/components/global-table.blade.php
+++ laravel/resources/views/livewire/components/global-table.blade.php
@@ -4,24 +4,7 @@
         <x-form.date-range :date_range="$date_range" placeholder="Filter irrigation data..." />
 
         {{-- Show All / Reset --}}
-        <button wire:click="showAll" @click='dateInput.clear()' type="button"
-            class="
-            inline-flex items-center justify-center
-            gap-2
-            px-4 py-2
-            rounded-2xl
-            bg-emerald-50
-            border border-emerald-200
-            text-sm font-medium text-emerald-700
-            hover:bg-emerald-100
-            hover:border-emerald-300
-            active:scale-[0.98]
-            transition-all cursor-pointer
-            whitespace-nowrap
-        ">
-            <x-icons.refresh wire:loading.class='animate-spin' wire:target='showAll' size="16" />
-            <span>Show All</span>
-        </button>
+        <x-ui.button.show-all click="showAll" @click='dateInput.clear()' />
     </div>
 
     {{-- Table --}}
