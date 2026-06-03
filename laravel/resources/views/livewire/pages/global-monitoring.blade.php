821927e62234f38090897fbcda5659311c343b27
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-06-03 20:12:37 +0700

    feat: light intensity chart

--- laravel/resources/views/livewire/pages/global-monitoring.blade.php
+++ laravel/resources/views/livewire/pages/global-monitoring.blade.php
@@ -6,13 +6,18 @@
 
         <x-card.global-cards :global-data="$data" />
 
-        <!-- Charts -->
-        <div class="grid md:grid-cols-2 gap-6">
+        <!-- Global Data Visualization -->
+        <div class="grid md:grid-cols-2 gap-6 mb-6">
+            {{-- pH Chart --}}
             <livewire:components.line-chart lazy field="ph" fieldName="pH" table="global" />
+            {{-- Water Flow Chart --}}
             <livewire:components.line-chart lazy field="water_flow" fieldName="Water Flow" table="global"
                 ylabel=' L/m' />
-            <x-container title="Global Periodic Table" description='Riwayat data global secara periodik'
-                class="col-span-full">
+            {{-- Water Flow Chart --}}
+            <livewire:components.line-chart lazy field="light" fieldName="Light Intensity" table="global"
+                ylabel='' />
+            {{-- Table --}}
+            <x-container title="Global Periodic Table" description='Riwayat data global secara periodik'>
                 <livewire:components.global-table lazy />
             </x-container>
         </div>
