c8ce810a7bbe748dfa514a775deba2079615e004
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-06-12 21:43:22 +0700

    feat: prediction use model

--- fastapi/model/prediction.py
+++ fastapi/model/prediction.py
@@ -1,30 +1,51 @@
 import random
 import time
 
+import pandas as pd
+import numpy as np
+from keras.models import load_model
+import joblib
+
+# Load scaler dan model yang sudah dilatih
+scaler = joblib.load('model/scaler.pkl')
+model = load_model('model/best_model.keras')
+
+features = ['moisture_after', 'moisture_before', 'moisture_gain', 'moisture_rate','valve_duration']
+mse_threshold = 1.28228
+
+
 def predictGlobalAnomaly():
     return True
 
 def predictTreeAnomaly():
     return True
 
-def predictEventSystem(data):
+def predictEventSystem(data: dict):
     time_start = int(time.time() * 1000)
-    feature = ['moisture_before', 'moisture_after', 'moisture_gain', 'moisture_rate', 'valve_duration']
     
-    global_mse = round(random.uniform(0, 2), 3)
-    threshold = 1
-    fault_ratio = global_mse / threshold
-    sleep_time = round(random.uniform(0.5, 2), 2)
-    print(f"Simulating prediction time of {sleep_time} seconds...")
-    time.sleep(sleep_time)
+    df = pd.DataFrame([data], columns=features)
+    print('Input df:', df)
+    scaler_df = scaler.transform(df)
+    
+    # Predict the class
+    recon_data = model.predict(scaler_df)
+    error_data = np.abs(scaler_df - recon_data)
+    mean_error = np.mean(error_data)
+    fault_ratio = mean_error / mse_threshold
+    max_error = np.max(error_data)
+    
     prediction_time = int(time.time() * 1000) - time_start
     return {
-        **data,
-        'global_mse': global_mse,
-        'fault_ratio': fault_ratio,
+        'event_id': data['event_id'],
+        'node_id': data['node_id'],
+        'tree_id': data['tree_id'],
+        'global_mse': round(mean_error, 2),
+        'fault_ratio': round(fault_ratio, 2),
         'flag': fault_ratio >= 1,
-        'dominant_feature': random.choice(feature),
-        'dominant_ratio': round(random.uniform(50, 80), 1),
+        'severity': 'high' if fault_ratio >= 1 else 'low',
+        'dominant_feature': features[np.argmax(error_data)],
+        'dominant_error': round(max_error, 2),
+        'dominant_ratio': round(max_error / np.sum(error_data), 2),
         'prediction_time': prediction_time,
         'time': int(time.time() * 1000),
     }
